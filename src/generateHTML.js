2bbb1823664c96e6e38f2e7802796ea4fd29de8c
SJohnRose <shamini.johnrose@gmail.com>
2023-02-18 22:21:57 +1100

    Updated generateHTML.js

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,17 +1,19 @@
 const employeeFile = require('fs');
 const fileName = "team.html";
 
-var employeeData, employeeData1, employeeData2, employeeData2a, employeeData234 = " ", employeeData3, employeeData4, employeeData5;
+var employeeHTML, empHTMLStart, empDetails, empData, empCard = " ", empEndDiv, empEndBody, empNameIcon;
 var currentRole;
 var icon;
 
 function generateHTML(EmployeeObj) {
-    employeeData1 = `<!DOCTYPE html>
+    //  HTML for the head and top of body section
+    empHTMLStart = `<!DOCTYPE html>
     <html lang="en">
     
     <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
       <link rel="stylesheet" href="./src/style.css" />
       <title>Team Profile</title>
     </head>
@@ -25,11 +27,14 @@ function generateHTML(EmployeeObj) {
     
         <div class="main">
                 <div id="employee-section">`;
+    // HTML for generating a card for each employee in team
     for(i=0; i<EmployeeObj.length; i++) {
         currentRole = EmployeeObj[i].getRole();
-        employeeData2 = 
+        empNameIcon = 
                     `<div class="employee-card">
-                        <h1 id="employee-name"> ${EmployeeObj[i].name} </h1>`
+                        <div class = "emp-header mb-3">
+                        <h3 id="employee-name"> ${EmployeeObj[i].name} </h3>
+                        <div class = "sub-header">`
                         if (currentRole == "Manager"){
                             icon = "assets/manager.png";}
                         else if (currentRole == "Engineer"){
@@ -38,25 +43,28 @@ function generateHTML(EmployeeObj) {
                         else if (currentRole == "Intern"){
                             icon = "assets/intern.png";
                         }
-        employeeData2a =
-                        `<img id="role-icon" width="25" height="25" src = "${icon}"/>
-                        <h3 id="employee-role">${EmployeeObj[i].getRole()}</h3>
-                        <h3 id="employee-id">ID: ${EmployeeObj[i].id}</h3>
-                        <h3 id="employee-email">Email: ${EmployeeObj[i].email}</h3>`
+        empDetails =
+                        `<img id="role-icon" width="50" height="50" src = "${icon}"/>
+                        <h5 id="employee-role">${EmployeeObj[i].getRole()}</h5>
+                            </div>
+                        </div>
+                        <div class="bottom-section">
+                        <h5 id="employee-id">ID: ${EmployeeObj[i].id}</h5>
+                        <h5 id="employee-email">Email: <a href = "mailto: ${EmployeeObj[i].email}">${EmployeeObj[i].email}</a></h5>`
                         if (currentRole == "Manager"){
-                            employeeData3 = `<h3 id="employee-officeNumber">Office Number: ${EmployeeObj[i].officeNumber}</h3>`
+                            empData = `<h5 id="employee-officeNumber">Office Number: ${EmployeeObj[i].officeNumber}</h5>`
                         }
                         else if (currentRole == "Engineer"){
-                            employeeData3 = `<h3 id="employee-github">Github Name: ${EmployeeObj[i].github}</h3>`
+                            empData = `<h5 id="employee-github">Github Name: <a href = "http://github.com/${EmployeeObj[i].github}">${EmployeeObj[i].github}</a> </h5>`
                         }
                         else if (currentRole == "Intern"){
-                            employeeData3 = `<h3 id="employee-school">School Name: ${EmployeeObj[i].school}</h3>`
+                            empData = `<h5 id="employee-school">School Name: ${EmployeeObj[i].school}</h5>`
                         }
 
-        employeeData4 = `</div>` ;
-        employeeData234 = employeeData234 + employeeData2 + employeeData2a + employeeData3 + employeeData4;
+        empEndDiv = `</div> </div>` ;
+        empCard = empCard + empNameIcon + empDetails + empData + empEndDiv;
     }                
-    employeeData5 =             
+    empEndBody =             
                 `</div>
             </div>
         </div>
@@ -64,8 +72,8 @@ function generateHTML(EmployeeObj) {
     </body>
     
     </html>`;
-    employeeData = employeeData1 + employeeData234 + employeeData5;
-    employeeFile.writeFileSync(fileName, employeeData, (err) =>
+    employeeHTML = empHTMLStart + empCard + empEndBody;
+    employeeFile.writeFileSync(fileName, employeeHTML, (err) =>
        err ? console.error(err) : console.log('Success!')
     );
     process.exit();
